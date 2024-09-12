9886e563d084589f6eb6c0017e5659fb33dd038c
LichRich <leech135789@gmail.com>
2024-09-12 16:29:33 +0900

    update for css
    - 화면 사이즈에 맞게 ui수정
    - ppt같아 보이지 않도록 해보려는중

--- src/components/Intro/Vision.jsx
+++ src/components/Intro/Vision.jsx
@@ -15,7 +15,36 @@ export default function Vision() {
                 </p>
             </div>
             <div className={styles.visionContentBox}>
-                <img src={process.env.PUBLIC_URL + '/logo_box.png'} alt="vision img" className={styles.visionImg} />
+                <div className={[styles.visionContents, styles.leftBox].join(' ')}>
+                    <div className={styles.visionImgBox}>
+                        <img src={process.env.PUBLIC_URL + '/logo_box.png'} alt="vision img" className={styles.visionImg} />
+                    </div>
+                    <div className={styles.visionContentsDescBox}>
+                        <p className={styles.visionContentsDesc}>
+                            우리는 이러이러한 것들을 추구하며 나아갑니다.
+                        </p>
+                    </div>
+                </div>
+                <div className={[styles.visionContents, styles.rightBox].join(' ')}>
+                    <div className={styles.visionContentsDescBox}>
+                        <p className={styles.visionContentsDesc}>
+                            우리는 지금도 어떤 것들을 바라보며 일을 하고 있고,
+                        </p>
+                    </div>
+                    <div className={styles.visionImgBox}>
+                        <img src={process.env.PUBLIC_URL + '/logo_box.png'} alt="vision img" className={styles.visionImg} />
+                    </div>
+                </div>
+                <div className={[styles.visionContents, styles.leftBox].join(' ')}>
+                    <div className={styles.visionImgBox}>
+                        <img src={process.env.PUBLIC_URL + '/logo_box.png'} alt="vision img" className={styles.visionImg} />
+                    </div>
+                    <div className={styles.visionContentsDescBox}>
+                        <p className={styles.visionContentsDesc}>
+                            앞으로도 어떠한 방향성을 갖고 이렇게저렇게 나아갈 것입니다.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
